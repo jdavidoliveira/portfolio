a5cfe8ea0a9e6eef782203fe224ca1aa431f9d48
lordaval <lordaval1117@gmail.com>
2025-01-22 14:30:03 -0300

    fix: improve clarity and grammar in experience descriptions

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -6,7 +6,7 @@ export const experience: IExperience[] = [
     roll: 'Suporte de TI',
     date: '02/2024 - Atual',
     location: 'Presencial - Rio de Janeiro - RJ',
-    description: `Atuo com desenvolvimento de software e gestão de plataformas, além de ajudar e auxiliar outros funcionários a usarem a plataforma e os softwares desenvolvidos por nós.`
+    description: `Atuo desenvolvendo software e gerindo plataformas, além de ajudar e auxiliar outros funcionários a usarem a plataforma e os softwares desenvolvidos por nós.`
   },
   {
     company: 'GMR Pesquisas',
@@ -14,13 +14,13 @@ export const experience: IExperience[] = [
     date: '01/2024 - 12/2024',
     location: 'Remoto - Rio de Janeiro - RJ',
     description:
-      'Coordeno e desenvolvo um CRM para a realização de tarefas dentro da empresa. Algumas dessas tarefas incluem a importação de um grande volume de dados para um banco próprio, através de um processamento e otimização de dados.'
+      'Coordenei e desenvolvi um CRM para a realização de tarefas dentro da empresa. Algumas dessas tarefas incluem a importação de um grande volume de dados para um banco próprio com processamento e otimização de dados.'
   },
   {
     company: 'Colégio SEICE',
     roll: 'Estágio de TI',
     date: '06/2023 - 11/2023',
     location: 'Híbrido - Rio de Janeiro - RJ',
-    description: `Atuei desenvolvendo novas aplicações, e implementando aplicações ja existentes. Desde aplicações desktop para problemas internos, como tratamento de imagens, e controle de avaliações de alunos, até aplicações que consumiam API's REST e SOAP.`
+    description: `Desenvolvi diversas aplicações para automatizar tarefas, e complementei aplicações ja existentes, desde aplicações desktop para problemas internos como tratamento de imagens e controle de avaliações de alunos, até aplicações que consumiam API's REST e SOAP.`
   }
 ]
